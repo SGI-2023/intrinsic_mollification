668f98cc70642434aa102e8dbf25f7471b60da26
ricGLo <ricalgp00@gmail.com>
2023-08-01 13:15:25 -0600

    modify mollification function so it returnts lenghts in a #F x 3 shape

--- src/Mollification.py
+++ src/Mollification.py
@@ -6,7 +6,6 @@
 
 '''
 
-import numpy as np
 import igl
 
 '''
@@ -16,21 +15,18 @@ import igl
         delta: such that l_ij + l_jk > l_ki + delta
 
     Returns:
-        E: #E x 2 list of edges in no particular order
-        newL: array of length #E with new lengths corresponding to the same order of E
+        L: columns correspond to original edges lengths [1,2],[2,0],[0,1]
         eps = max_T max(0, delta - l_ij - l_jk - l_ki )
+        newL: new edges lengths in the same shape as L      
 '''
-def IntrinsicMollification(V, F, delta = 10e-4):
-    L = igl.edge_lengths(V, F)         # columns correspond to edges lengths [1,2],[2,0],[0,1]
-    E = igl.edges(F)                   # #E x 2 list of edges in no particular order
 
+def IntrinsicMollification(V, F, delta = 1e-4):
+    L = igl.edge_lengths(V, F)         # columns correspond to edges lengths [1,2],[2,0],[0,1]
     eps = 0.0
+
     # iterate over each triangle to compute epsilon
     for T in L:
-        eps = max(   [0, eps, delta + T[0] - T[1] - T[2], delta - T[0] + T[1] - T[2], delta - T[0] - T[1] + T[2] ]  )
+        eps = max(   [eps, delta + T[0] - T[1] - T[2], delta - T[0] + T[1] - T[2], delta - T[0] - T[1] + T[2] ]  )
 
-    newL = eps*np.ones(len(E))
-    newL = newL + igl.edge_lengths(V, E)
-
-    return E, newL, eps
-
+    newL = eps + L
+    return L, eps, newL
