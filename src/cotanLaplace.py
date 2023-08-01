5a62dc06bbbc3689a143682ce545296ca9e3a8c1
hoskillua <47090776+hoskillua@users.noreply.github.com>
2023-08-01 15:10:14 +0300

    phase 2 incomplete

--- src/cotanLaplace.py
+++ src/cotanLaplace.py
@@ -18,29 +18,30 @@ import Mollification
 '''
 def cotanLaplace(V, F, delta = 10e-4):
     E, newL, eps = Mollification.IntrinsicMollification(V, F, delta)
-    Laplacian = np.zeros(E.shape[0], E.shape[0])
 
-    print("E = ", E)
-    print("Laplacian = ", Laplacian)
+    Laplacian = np.zeros((E.shape[0], E.shape[0]))
 
-    # # Build the cotan-Laplace matrix from the intrinsic lengths
-    # for fr in F:
-    #     for i in range(3):
-    #         # get the edge index
-    #         e = E.index([fr[i], fr[(i+1)%3]])
-    #         # get the edge length
-    #         l = newL[e]
-    #         # get the other two vertices
-    #         v1 = fr[i]
-    #         v2 = fr[(i+1)%3]
-    #         v3 = fr[(i+2)%3]
-    #         # get the cotan of the angle
-    #         cot = igl.cotmatrix_entries(V, F)[e]
-    #         # update the cotan-Laplace matrix
-    #         Laplacian[v1, v2] += cot/l
-    #         Laplacian[v2, v1] += cot/l
-    #         Laplacian[v1, v1] -= cot/l
-    #         Laplacian[v2, v2] -= cot/l
+    # sort edge entries so that E[:,0] < E[:,1]
+    E = np.sort(E, axis = 1)
 
+    # Build the cotan-Laplace matrix from the intrinsic lengths
+    for fr in F:
+        # for each face, get the 3 edge indices
+        e1 = np.where((E[:,0] == min(fr[i], fr[(i+1)%3])) & (E[:,1] == max(fr[i], fr[(i+1)%3])))[0][0]
+        e2 = np.where((E[:,0] == min(fr[(i+1)%3], fr[(i+2)%3])) & (E[:,1] == max(fr[(i+1)%3], fr[(i+2)%3])))[0][0]
+        e3 = np.where((E[:,0] == min(fr[(i+2)%3], fr[i])) & (E[:,1] == max(fr[(i+2)%3], fr[i])))[0][0]
 
+        l1 = newL[e1]
+        l2 = newL[e2]
+        l3 = newL[e3]
+
+        s = (l1 + l2 + l3)/2
+        area = np.sqrt(s*(s-l1)*(s-l2)*(s-l3))
+
+        # get the cotan of the angle
+        faceL = [l1, l2, l3]
+        for i in range(3):
+            # get w for corner
+
+            # update the cotan-Laplace matrix
 
